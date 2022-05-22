9d4e93a93e914949517a2f9c6ef3c4a2d805fed4
RTMK <58647967+Sanzipchhetri@users.noreply.github.com>
2022-05-22 20:15:54 +0545

    New

---
+++ src/data/WorkData.js
@@ -0,0 +1,38 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Global Village mart",
+        description:"Global Village Mart is Offline retail chain & Online Marketplace platform for customers to buy all the daily necessities and durable goods..",
+        tags:["mart","onlinestore","groceries"],
+        
+        demo:"https://www.facebook.com/GlobalVillageMart",
+    },
+    {
+        id:2,
+        name:"Urbar bazaar",
+        description:"Urbar Nepal Public Limited is a legal entity formed by collective and synergic effort of farmer.",
+        tags:["organic","online"],
+        
+        demo:"https://www.facebook.com/urbarnepa",
+        
+    },
+    {
+        id:3,
+        name:"Online Sofa",
+        description:"A complete online store for sofa and chair.",
+        tags:["sofa","online","store"],
+        
+        demo:"https://www.facebook.com/onlinesofacentre",
+    },
+    {
+        id:4,
+        name:"Darshan solution",
+        description:"Your complete IT solutions.",
+        tags:["webdev","graphics","appdev"],
+        
+        demo:"https://www.facebook.com/darshansolutions",
+       
+    },
+] 
